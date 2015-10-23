2d62d26e90b2aa23fcaf3b48d6faac4ef40adb82
Brian Galey <bkgaley@gmail.com>
2015-10-22 19:54:18 -0700

    No need for dataset cpointer type, use ptr _int

--- netcdf/ffi.rkt
+++ netcdf/ffi.rkt
@@ -12,9 +12,6 @@
 
 (define-ffi-definer define-netcdf (ffi-lib "libnetcdf"))
 
-(define-cpointer-type _dataset-ptr)
-(define _variable _int)
-
 ;; Return the NetCDF library version.
 (define-netcdf nc_inq_libvers
   (_fun -> _string))
@@ -88,24 +85,24 @@
 (define-netcdf nc_open
   (_fun (filename : _file)
         (mode : _open-mode)
-        (ncid : (_ptr o _dataset-ptr))
+        (ncid : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_open) ncid)))
 
 (define-netcdf nc_create
   (_fun (filename : _file)
         (mode : _create-mode)
-        (ncid : (_ptr o _dataset-ptr))
+        (ncid : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_create) ncid)))
 
 (define-netcdf nc_close
-  (_fun _dataset-ptr
+  (_fun (ncid : _int)
         -> (result : _int)
         -> (check result 'nc_close)))
 
 (define-netcdf nc_def_dim
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (dimname : _string)
         (dimlen : _size)
         (dimid : (_ptr o _int))
@@ -113,26 +110,26 @@
         -> (and (check result 'nc_def_dim) dimid)))
 
 (define-netcdf nc_def_var
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (varname : _string)
         (dtype : _data-type)
         (_int = (length dimlist))
         (dimlist : (_list i _int))
-        (varid : (_ptr o _variable))
+        (varid : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_def_var) varid)))
 
 (define-netcdf nc_def_var_chunking
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (storage : _storage-type)
         (chunk-size : (_list i _size))
         -> (result : _int)
         -> (check result 'nc_def_var_chunking)))
 
 (define-netcdf nc_def_var_deflate
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (shuffle? : _int)
         (deflate? : _int)
         (deflate-level : _int)
@@ -140,13 +137,13 @@
         -> (check result 'nc_def_var_deflate)))
 
 (define-netcdf nc_inq_format
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (nc-format : (_ptr o _dataset-type))
         -> (result : _int)
         -> (and (check result 'nc_inq_format) nc-format)))
 
 (define-netcdf nc_inq_format_extended
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (nc-format : (_ptr o _dataset-extended-type))
         (mode : (_ptr o _create-mode))
         -> (result : _int)
@@ -154,7 +151,7 @@
                 (values nc-format mode))))
 
 (define-netcdf nc_inq
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (ndims : (_ptr o _int))
         (nvars : (_ptr o _int))
         (natts : (_ptr o _int))
@@ -164,27 +161,27 @@
                 (values ndims nvars natts unlimdim))))
 
 (define-netcdf nc_inq_nvars
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (nvars : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_inq_nvars) nvars)))
 
 (define-netcdf nc_inq_attlen
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (name : _string)
         (len : (_ptr o _size))
         -> (result : _int)
         -> (and (check result 'nc_inq_attlen) len)))
 
 (define-netcdf nc_inq_ndims
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (ndims : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_inq_ndims) ndims)))
 
 (define-netcdf nc_inq_dim
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (dimid : _int)
         (dimname : (_bytes o NC_MAX_NAME))
         (dimlen : (_ptr o _size))
@@ -193,15 +190,15 @@
                 (values (cast dimname _bytes _string) dimlen))))
 
 (define-netcdf nc_inq_dimid
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (dimname : _string)
         (dimid : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_inq_dimid) dimid)))
 
 (define-netcdf nc_inq_var
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (varname : (_bytes o NC_MAX_NAME))
         (dtype : (_ptr o _data-type))
         (ndims : (_ptr o _int))
@@ -213,15 +210,15 @@
                         dtype ndims dimlist natts))))
 
 (define-netcdf nc_inq_varid
-  (_fun (ncid : _dataset-ptr)
+  (_fun (ncid : _int)
         (varname : _string)
-        (varid : (_ptr o _variable))
+        (varid : (_ptr o _int))
         -> (result : _int)
         -> (and (check result 'nc_inq_varid) varid)))
 
 (define-netcdf nc_get_att_text
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (name : _string)
         (value : (_bytes o (nc_inq_attlen ncid varid name)))
         -> (result : _int)
@@ -229,8 +226,8 @@
 
 ;; Read entire var in a single call.
 (define-netcdf nc_get_var
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (vec : (_cvector i))
         -> (result : _int)
         -> (and (check result 'nc_get_var) vec)))
@@ -238,8 +235,8 @@
 ;; Read an array of values from a variable, converts data to output type as
 ;; needed.
 (define-netcdf nc_get_vara_float
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (start : (_list i _size))
         (counts : (_list i _size))
         (size : _? = (apply * (map - counts start)))
@@ -248,8 +245,8 @@
         -> (and (check result 'nc_get_vara_float) vec)))
 
 (define-netcdf nc_put_att_text
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (name : _string)
         (_size = (string-length value))
         (value : _string)
@@ -258,15 +255,15 @@
 
 ;; Write the entire var with one call.
 (define-netcdf nc_put_var
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (arr : (_cvector i))
         -> (result : _int)
         -> (check result 'nc_put_var)))
 
 (define-netcdf nc_put_vara_float
-  (_fun (ncid : _dataset-ptr)
-        (varid : _variable)
+  (_fun (ncid : _int)
+        (varid : _int)
         (start : (_list i _size))
         (counts : (_list i _size))
         (arr : (_cvector i))
